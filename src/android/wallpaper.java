8d3f6d7b6fe09c71324c4796cbcbbff15ab4945f
flyingPotat0e <jan.baudisch@live.com>
2016-07-28 15:36:23 +0200

    Update wallpaper.java
    added Base64 support

--- src/android/wallpaper.java
+++ src/android/wallpaper.java
@@ -28,13 +28,15 @@ public class wallpaper extends CordovaPlugin {
 		
 		context = IS_AT_LEAST_LOLLIPOP ? cordova.getActivity().getWindow().getContext() : cordova.getActivity().getApplicationContext();
 		String imgSrc = "";
+		Boolean base64 = false;
 		
         if (action.equals("start")) {
 			for (int i = 0; i < args.length(); i++) {
 				JSONObject jsonobject = args.getJSONObject(i);
 				imgSrc = jsonobject.getString("image");
+				base64 = jsonobject.getBoolean("base64");
 			}
-            this.echo(imgSrc, context);
+            this.echo(imgSrc, base64, context);
 			PluginResult pr = new PluginResult(PluginResult.Status.OK);
           	pr.setKeepCallback(true);
           	callbackContext.sendPluginResult(pr);
@@ -45,11 +47,18 @@ public class wallpaper extends CordovaPlugin {
         return false;
     }
 
-    public void echo(String image, Context context) {
+    public void echo(String image, Boolean base64, Context context) {
 		try {
 			AssetManager assetManager = context.getAssets();
-			InputStream instr = assetManager.open("www/" + image);
-			Bitmap bitmap = BitmapFactory.decodeStream(instr);
+			Bitmap bitmap;
+			if(base64) { //Base64 encoded
+				byte[] decoded = Base64.decode(image, Base64.DEFAULT);
+				bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+			}
+			else { //normal path
+				InputStream instr = assetManager.open("www/" + image);
+				bitmap = BitmapFactory.decodeStream(instr);
+			}
 			WallpaperManager myWallpaperManager	= WallpaperManager.getInstance(context);
 			myWallpaperManager.setBitmap(bitmap);
 		} catch (IOException e) {
