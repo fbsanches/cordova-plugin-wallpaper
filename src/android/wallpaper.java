df4304e59c80058e09e9466adff1216c99b8db3a
flyingPotat0e <jan.baudisch@live.com>
2016-07-30 16:18:26 +0200

    Update wallpaper.java

--- src/android/wallpaper.java
+++ src/android/wallpaper.java
@@ -49,7 +49,7 @@ public class wallpaper extends CordovaPlugin
 			Bitmap bitmap;
 			if(base64) //Base64 encoded
 			{
-				byte[] decoded = Base64.decode(image.getBytes());
+				byte[] decoded = util.Base64.decode(image.getBytes());
 				bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
 			}
 			else //normal path
